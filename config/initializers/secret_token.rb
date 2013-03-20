d33fa32bd1b3709f2bb7c43a5146e40ea7a12d71
Yichen Yu <chouchangke525@yahoo.com.cn>
2013-03-20 17:48:12 -0500

    create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sudoku::Application.config.secret_token = '1d635e6fc7abe297e2511842b1ba009be97edfad9741419ac8140422b2989fce40a228bde542d3fc19d9f8bec2ca0495a2bb05e5419e2a66b09753e1b637edd3'
